a62550cb5e673e3296c2f5aa425177de0b2d0ae2
turtle-king <mianhua9999@gmail.com>
2022-07-10 18:35:28 -0700

    add

---
+++ src/constants/address.js
@@ -0,0 +1,3 @@
+// export const contractAddress = "0x2eF7833174c4C0d51d2716e88779a01F4a397068";
+export const contractAddress = "0xa2e00990cf5e2ee8b63e390ac3b14e0d735ec245";
+export const chainId = "0x4";
